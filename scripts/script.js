3b751fdae6d952f17bfa1512d3b7f93ec8e5383f
Glitch (glitch-hello-website) <none>
2024-11-21 04:27:08 +0000

    🍥📡 Checkpoint ./scripts/script.js:64179039/1645 ./index.html:64179039/12674

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
+// page load
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.querySelector('.search-button');
+    const infoTable = document.querySelector('.info-table');
+
+    // click search button
+    searchButton.addEventListener('click', () => {
+        if (infoTable) {
+            infoTable.style.display = 'table'; // show table
+        }
+    });
+});
+
+
+/*
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.querySelector('.search-button');
     if (searchButton) {
@@ -7,3 +22,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+*/
+

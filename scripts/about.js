454d52f43530d8f5fd79629f5da48578fa531907
Glitch (glitch-hello-website) <none>
2024-12-07 22:07:29 +0000

    🐘👰 Checkpoint ./styles/styles.css:64179039/490 ./scripts/about.js:64179039/34

--- scripts/about.js
+++ scripts/about.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const answer = question.nextElementSibling;
       const isExpanded = question.getAttribute("aria-expanded") === "true";
 
-      // 切換 aria-expanded 和顯示隱藏內容
       question.setAttribute("aria-expanded", !isExpanded);
       answer.hidden = isExpanded;
     });
